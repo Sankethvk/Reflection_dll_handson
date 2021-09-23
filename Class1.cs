43a76e2df1551b62ec179139d23a0b5c15d56ffd
SanketV <sankethvivek@gmail.com>
2021-09-23 18:15:45 +0530

    ReFlection concept , accessing metadata using Reflection

---
+++ Class1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace DLL_File
+{
+    public class Class1
+    {
+
+        private int _num1 = 0;
+        private int _num2 = 0;
+
+        public void ClearResults()
+        {
+            _num1 = 0;
+            _num2 = 0;
+        }
+
+        public int SetNum1
+        {
+            set
+            {
+                _num1 = value;
+            }
+            get
+            {
+                return _num1;
+            }
+        }
+
+        public double Sum(int a, int b)
+        {
+            _num1 = a;
+            _num2 = b;
+
+            return _num1 + _num2;
+
+        }
+
+        public double Sub(int a, int b)
+        {
+            _num1 = a;
+            _num2 = b;
+
+            return _num1 - _num2;
+
+        }
+
+
+        public double Mul(int a, int b)
+        {
+            _num1 = a;
+            _num2 = b;
+
+            return _num1 * _num2;
+
+        }
+
+        public double Div(int a, int b)
+        {
+            _num1 = a;
+            _num2 = b;
+
+            return _num1 / _num2;
+
+        }
+
+        public double AreaofCircle(int radius)
+        {
+            double value1 = Math.Pow(radius, 2);
+            return 3.14 * value1;
+
+        }
+
+
+        public double AreaofTriangle(int _base, int _height)
+        {
+            _num1 = _base;
+            _num2 = _height;
+
+            return _num1 * _num2;
+
+        }
+
+
+        public double AreaofSphere(int radius)
+        {
+            double value1 = Math.Pow(radius, 2);
+            return 4 * 3.14 * value1;
+
+        }
+
+        public double VolumeaofSphere(int radius)
+        {
+            double value1 = Math.Pow(radius, 2);
+            return (4 / 3) * (3.14 * value1);
+
+        }
+
+
+        public void PrintClassName()
+        {
+
+            Console.WriteLine("clsCalculation...Printing");
+
+        }
+
+
+
+
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+
+namespace Reflection_dll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly myAssembly = Assembly.LoadFile(@"C:\Users\Sanket V\source\repos\DLL_File\DLL_File\bin\Debug\netcoreapp3.1\DLL_File.dll");
+
+            Type myType = myAssembly.GetType("DLL_File.Class1");
+
+            object objInstance = Activator.CreateInstance(myType);
+
+            Type objectTypes = objInstance.GetType();
+            Console.WriteLine("Reading metadata of an assembly via Reflection");
+            Console.WriteLine("************************");
+            foreach (MemberInfo objMemberInfo in objectTypes.GetMembers())
+            {
+                Console.WriteLine(objMemberInfo.Name);
+            }
+            Console.WriteLine("************************");
+            Console.WriteLine("Invoking particular method via Reflection");
+            objectTypes.InvokeMember("PrintClassName", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.InvokeMethod | BindingFlags.Instance, null, objInstance, null);
+
+            Console.WriteLine("************************");
+            Console.WriteLine("Invoking particular method with inputs using Reflection");
+            double answer = (double)objectTypes.InvokeMember("Sum", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.InvokeMethod | BindingFlags.Instance, null, objInstance, new object[] { 20, 10 });
+
+            Console.WriteLine(answer);
+
+            //Setting value to a property
+            PropertyInfo numberPropertyInfo = objectTypes.GetProperty("SetNum1");
+            numberPropertyInfo.SetValue(objInstance, 10, null);
+
+            //Getting value of a property
+            //PropertyInfo numberPropertyInfo = objectTypes.GetProperty("SetNum1");
+            //int value = (int)numberPropertyInfo.GetValue(objInstance, null);
+        }
+    }
+}
